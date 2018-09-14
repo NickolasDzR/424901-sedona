1b2db4178861c44e756667e32f85638b8e0bf0ca
Nikolai Erashov <nikolyae@mail.ru>
2018-09-14 12:10:49 +0300

    форматирование

--- js/script.js
+++ js/script.js
@@ -5,41 +5,41 @@ var text = popup.querySelector("[name=date-arrival]");
 var number = popup.querySelector("[name=number]");
 
 document.addEventListener("DOMContentLoaded", function () {
-	form.classList.add("form-show");
+  form.classList.add("form-show");
 });
 
 button.addEventListener('click', function (evt) {
-	evt.preventDefault();
-	popup.classList.toggle("animation-form");
-	text.focus();
+  evt.preventDefault();
+  popup.classList.toggle("animation-form");
+  text.focus();
 
 });
 
 window.addEventListener("keydown", function (evt) {
-	if (evt.keyCode === 27) {
-		if (popup.classList.contains("form-show")) {
-			evt.preventDefault();
-			popup.classList.remove("form-unshow");
-		}
-	}
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("form-show")) {
+      evt.preventDefault();
+      popup.classList.remove("form-unshow");
+    }
+  }
 });
 
 
 button.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popup.classList.remove("modal-show");
-	popup.classList.remove("modal-error");
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
-	if (!text.value || !number.value) {
-		evt.preventDefault();
-		popup.classList.remove("modal-error");
-		popup.offsetWidth = popup.offsetWidth;
-		popup.classList.add("modal-error");
-	} else {
-		if (isStorageSupport) {
-			localStorage.setItem("text", text.value);
-		}
-	}
+  if (!text.value || !number.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("text", text.value);
+    }
+  }
 });
